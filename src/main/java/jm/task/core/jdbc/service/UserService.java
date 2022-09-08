173572c2557f18a80f60a94de09e113daaaec62b
Slava <loginmail2018@inbox.ru>
2022-09-08 20:28:17 +0300

    реализация методов перенесена в интрефейс UserService

--- src/main/java/jm/task/core/jdbc/service/UserService.java
+++ src/main/java/jm/task/core/jdbc/service/UserService.java
@@ -1,21 +1,38 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface UserService extends UserDao {
-    void createUsersTable() throws SQLException;
 
-    void dropUsersTable() throws SQLException;
+    UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
 
-    void saveUser(String name, String lastName, byte age) throws SQLException;
+    default void createUsersTable() throws SQLException {
+        userDaoJDBCImpl.createUsersTable();
+    }
 
-    void removeUserById(long id) throws SQLException;
+    default void dropUsersTable() throws SQLException {
+        userDaoJDBCImpl.dropUsersTable();
+    }
 
-    List<User> getAllUsers() throws SQLException;
+    default void saveUser(String name, String lastName, byte age) throws SQLException {
+        userDaoJDBCImpl.saveUser(name, lastName, age);
+    }
 
-    void cleanUsersTable() throws SQLException;
+    default void removeUserById(long id) throws SQLException {
+        userDaoJDBCImpl.removeUserById(id);
+    }
+
+    default List<User> getAllUsers() throws SQLException {
+        return userDaoJDBCImpl.getAllUsers();
+    }
+
+    default void cleanUsersTable() throws SQLException {
+        userDaoJDBCImpl.cleanUsersTable();
+    }
+
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,31 +8,4 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
-
-    public void createUsersTable() throws SQLException {
-        userDaoJDBCImpl.createUsersTable();
-    }
-
-    public void dropUsersTable() throws SQLException {
-        userDaoJDBCImpl.dropUsersTable();
-
-    }
-
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
-        userDaoJDBCImpl.saveUser(name, lastName, age);
-    }
-
-    public void removeUserById(long id) throws SQLException {
-        userDaoJDBCImpl.removeUserById(id);
-    }
-
-    public List<User> getAllUsers() throws SQLException {
-
-        return userDaoJDBCImpl.getAllUsers();
-    }
-
-    public void cleanUsersTable() throws SQLException {
-        userDaoJDBCImpl.cleanUsersTable();
-    }
 }
